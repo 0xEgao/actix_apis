c5379f620d18b0c171c6a83d2de9790a38bef778
Sarthak <polarb587@gmail.com>
2025-06-02 17:20:04 +0530

    date api done

--- mingle/src/main.rs
+++ mingle/src/main.rs
@@ -1,8 +1,17 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web::Data};
 
 pub mod models;
 pub mod routes;
 pub mod services;
+
+use routes::{
+    booking_routes::{cancel_booking, create_booking, get_bookings},
+    date_routes::create_date,
+    owner_routes::create_owner,
+};
+
+use services::db::Database;
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello from actix web")
@@ -10,13 +19,20 @@ async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hellofromsarthak))
-        .bind(("localhost", 5001))?
-        .run()
-        .await
-}
+    let db = Database::init().await;
+    let db_data = Data::new(db);
 
-#[get("/sarthak")]
-async fn hellofromsarthak() -> impl Responder {
-    HttpResponse::Ok().body("Hello from Sarthak")
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(hello)
+            .service(create_owner)
+            .service(create_date)
+            .service(create_booking)
+            .service(get_bookings)
+            .service(cancel_booking)
+    })
+    .bind(("localhost", 5001))?
+    .run()
+    .await
 }
