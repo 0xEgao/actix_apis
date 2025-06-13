7a80fd9cdae2ef61bd9e765c5be37b65f6560129
Sarthak <polarb587@gmail.com>
2025-06-13 23:37:15 +0530

    allowed fe origin

--- time-capsule/src/main.rs
+++ time-capsule/src/main.rs
@@ -64,7 +64,11 @@ async fn main() {
     };
 
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin(
+            "https://time-capsule-rusty.vercel.app/"
+                .parse::<HeaderValue>()
+                .unwrap(),
+        )
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE])
         .allow_credentials(true)
         .allow_methods([Method::GET, Method::POST, Method::PUT]);
